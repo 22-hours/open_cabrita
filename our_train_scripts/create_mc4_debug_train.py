e09ba2867e4ca5f8288053b72865324fc49f43fb
marcospiau <marcospiau123@gmail.com>
2023-06-23 21:06:19 +0000

    Update create_mc4_debug_train.py

--- our_train_scripts/create_mc4_debug_train.py
+++ our_train_scripts/create_mc4_debug_train.py
@@ -4,11 +4,17 @@ import datasets
 
 if __name__ == "__main__":
     # iretable dataset
-    ds = datasets.load_dataset('mc4', 'pt', split='train', streaming=True)
-    ds = ds.remove_columns(['url', 'timestamp']).take(100_000)
-    # convert to map-style to save to disk
-    ds = datasets.Dataset.from_list(list(ds))
-    ds.save_to_disk('mc4_train_debug')
+    # ds = datasets.load_dataset('mc4', 'pt', split='train', streaming=True)
+    # ds = ds.remove_columns(['url', 'timestamp']).take(100_000)
+    # # convert to map-style to save to disk
+    # ds = datasets.Dataset.from_list(list(ds))
+    # # ds.save_to_disk('mc4_train_debug')
 
-    # save in json format
-    ds.to_json('mc4_train_debug.jsonl')
+    # # save in json format
+    # ds.to_json('mc4_train_debug.jsonl')
+
+    # validation, full dataset
+    ds = datasets.load_dataset('mc4', 'pt', split='validation', streaming=True)
+    ds = ds.remove_columns(['url', 'timestamp'])
+    ds = datasets.Dataset.from_list(list(ds))
+    ds.to_json('mc4_validation_debug.jsonl')
